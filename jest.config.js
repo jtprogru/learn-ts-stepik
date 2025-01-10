78182a710de774f37f1ccfa28979c57537017fc6
Mikhail Savin <jtprogru@gmail.com>
2025-01-10 23:02:42 +0300

    Add reporting tools

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,12 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "./reports/junit", outputName: "results.xml" }],
+    ["jest-html-reporter", {
+      "pageTitle": "Test Report",
+      "outputPath": "./reports/html/report.html"
+    }]
+  ]
 };
